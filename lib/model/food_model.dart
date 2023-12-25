683f338d2a3bdcf844dea3f2743eeab03a1eedac
yacine37th <y.titouah@esi-sba.dz>
2023-12-25 23:22:51 +0100

    food model updated

--- lib/model/food_model.dart
+++ lib/model/food_model.dart
@@ -1,8 +1,13 @@
 class FoodModel {
   late String? uID;
+  late String? name;
+  late int? price;
+
   late String? image;
   FoodModel({
     required this.uID,
+    required this.name,
+    required this.price,
     required this.image,
   });
 }
