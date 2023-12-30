712f39b09c9eaef7b10c5a3aa7a293660398d60b
yacine37th <y.titouah@esi-sba.dz>
2023-12-30 12:24:23 +0100

    carteModel updated

--- lib/controller/carte_controller.dart
+++ lib/controller/carte_controller.dart
@@ -1,5 +1,7 @@
+import 'package:fluter_ecom/model/carte_model.dart';
 import 'package:get/get.dart';
 
 class CarteController extends GetxController {
+  Map<String , CarteModel> orederList = {};
   
-}
+  }
--- lib/model/carte_model.dart
+++ lib/model/carte_model.dart
@@ -1,15 +1,18 @@
 class CarteModel {
-  late String? imageUrl;
+  late String? id;
+  // late String? imageUrl;
+  late String? foodID;
   late int? qte;
-  late int? foodPrice;
-  late String? foodName;
+  // late int? foodPrice;
+  // late String? foodName;
   late String? userID;
   late bool? isConfirm;
   CarteModel({
-    required this.imageUrl,
+    required this.id,
+    required this.foodID,
     required this.qte,
-    required this.foodPrice,
-    required this.foodName,
+    // required this.foodPrice,
+    // required this.foodName,
     required this.userID,
     required this.isConfirm,
   });
